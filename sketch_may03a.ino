3b11833b9dc9d0d10ece46851e6a747841c09638
mininerej <mininerej@users.noreply.github.com>
2017-05-29 01:56:42 +0900

    DailyLook과 Closet 변수들과 함수들을 정리
    DailyLook과 Closet 변수들과 함수들을 정리하였습니다.
    아직 보드를 가지고 있지 않아 실행해보지는 못했습니다.
    내일 보드를 받고 각각 test해 볼 예정입니다.

--- sketch_may03a.ino
+++ sketch_may03a.ino
@@ -7,29 +7,44 @@
 #include <new.h>
 
 #include <Wire.h>                       /* I2C control library*/
-          /* LCD library */
+/* LCD library */
 
 #define    uchar    unsigned char
-#define    uint    unsigned int
+#define    uint     unsigned int
 
-class DailyLook{
-  private :
+class DailyLook {
+private :
+  static const byte COLOR_SIZE = 3; //0; 자켓, 1; 윗도리, 2; 바지
+
   String name;
-  int* category; // 0;jacket, 1;top, 2;pants, 3;skirt, 4;socks, 5;dress
-  int* colors;
-  int score = 0;
-  int temperBase;
-  int temperLimit;
-  int humidBase;
-  int humidLimit;
+  unsigned byte* category; // 0;jacket, 1;top, 2;pants, 3;skirt
+  unsigned byte* colors[COLOR_SIZE];
+  unsigned byte  score = 0;
+  byte temperBase;
+  byte temperLimit;
+  unsigned byte humidBase;
+  unsigned byte humidLimit;
   boolean currentUse = false;
-  
- public  :
+
+public :
   DailyLook (){
     Serial.println("no input!!");
- }
-  DailyLook(String name, int category[3], int colors[3], int temperBase, int temperLimit, int humidBase, int humidLimit):
-    name(name), category(category),colors(colors),temperBase(temperBase),temperLimit(temperLimit),humidBase(humidBase), humidLimit(humidLimit){}
+  }
+
+  DailyLook(String name, unsigned byte category[3], unsigned byte colors[COLOR_SIZE][3],
+            byte temperBase, byte temperLimit, unsigned byte humidBase, unsigned byte humidLimit)
+    : name(name)
+    , category(category)
+    , temperBase(temperBase)
+    , temperLimit(temperLimit)
+    , humidBase(humidBase)
+    , humidLimit(humidLimit) {
+
+    for (int i = 0; i < 3; ++i) {
+      
+    }
+  }
+
   String getName() {
     return name;
   }
@@ -85,78 +100,81 @@ class DailyLook{
     currentUse = currentUse;
   }
 };
- 
+
 class Closet {
-  private :
+private :
   String masterName;
   int masterUID;
-  DailyLook DailyLook();
-  DailyLook list[2];
+  DailyLook* list[20];
 
- public :
+public :
   Closet(String name, int UID):
-  masterName(name), masterUID(UID){}
+    masterName(name), masterUID(UID){}
 
   void toString(){
-    for(int i=0; i<20;i++) list[i].toString();
+    for(int i=0; i<20;i++) list[i]->toString();
   }
-   String getMasterName() {
-   return masterName;
- }
-   void setMasterName(String masterName) {
-   masterName = masterName;
- }
-   int getMasterUID() {
-   return masterUID;
- }
-   void setMasterUID(int masterUID) {
-   masterUID = masterUID;
- }
-   DailyLook* getList() {
-   return this->list;
- }
-   void setList(DailyLook* list) {
-   list = list;
- }
+  String getMasterName() {
+    return masterName;
+  }
+  void setMasterName(String masterName) {
+    masterName = masterName;
+  }
+  int getMasterUID() {
+    return masterUID;
+  }
+  void setMasterUID(int masterUID) {
+    masterUID = masterUID;
+  }
+  /*
+    DailyLook* getList() {
+    return this->list;
+    }
+  */
+  void setList(DailyLook* list[20]) {
+    for (int i=0; i<20; ++i){
+      this->list[i] = list[i];
+    }
+  }
  
 };
 
 class MusicLib{
   
-  public :
+public :
   /*
    * 
    void MusicLib(){}
   
-  void playMusic(String Title){
+   void playMusic(String Title){
     
-  }
+   }
   */
 };
 
 void printWeather(int temper, int humid){
-    /* print current Weather
-     *  
-     */
-  }
+  /* print current Weather
+   *  
+   */
+}
   
   
-  /*
+/*
   DailyLook chooseDailyLook(Closet person, int temper, int humid){
     
-     * DailyLook loook = new DailyLook();
-     * return loook;
+  * DailyLook loook = new DailyLook();
+  * return loook;
      
     
   }
-  */
-  void scoreDailyLook(DailyLook look){
-    /*
-     * TODO 점수 매기기
-     */
-  }
+*/
+void scoreDailyLook(DailyLook look){
+  /*
+   * TODO 점수 매기기
+   */
+}
 
- /* 
+/* 
  * -*-*-*-*-*-*-*-*-*-*-*-*-MAIN PART-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
  */
  
@@ -164,34 +182,48 @@ void printWeather(int temper, int humid){
 
 
 // for data
- int curtemper;
- int curhumid;
- String weatherMsg[10];
- MusicLib musics;/*
- DailyLook curChoices[];
- Closet closets[];
-*/
+int curtemper;
+int curhumid;
+String weatherMsg[10];
+MusicLib musics;/*
+                  DailyLook curChoices[];
+                  Closet closets[];
+                */
 
 int cate1[] =  {1,2};
-  int color1[2][3] = {{255,255,255},{0,0,255}};
-  int cate2[] = {1,3};
-  int color2[2][3] = {{0,0,0}, {255,0,0}};
-  DailyLook* dl1 = new DailyLook(String("Whiteshirt&bluepants"), cate1, color1,15, 28, 0, 100);
-  DailyLook* dl2 = new DailyLook(String("BlackSweater&redpants"), cate2, color2,0, 50, 0, 100);
-  DailyLook* MinsuList[] = {dl1, dl2};
-  
-  Closet MinsuCloset = new Closet("Minsu", 23);
-  MinsuCloset.setList(MinsuList);
-  
+int color1[2][3] = {{255,255,255},{0,0,255}};
+int cate2[] = {1,3};
+int color2[2][3] = {{0,0,0}, {255,0,0}};
+DailyLook dl1 = DailyLook(String("Whiteshirt&bluepants"), cate1, color1,15, 28, 0, 100);
+DailyLook dl2 = DailyLook(String("BlackSweater&redpants"), cate2, color2,0, 50, 0, 100);
+DailyLook* MinsuList[] = {&dl1, &dl2};
+
+Closet* minsuCloset = new Closet("Minsu", 23);
+
+
+/*
+    #include <Wire.h>
+    #define SLAVE 4 // 슬레이브 주소
+    void i2c_communication() {
+    Wire.requestFrom(SLAVE, 1); // 1 바이트 크기의 데이터 요청
+    char c = Wire.read(); // 수신 데이터 읽기
+    Serial.println(c); // 수신 데이터 출력
+    }
+*/
+
 void setup(){
-  Serial.begin(9600);
+  Wire.begin(); // Wire 라이브러리 초기화
+  Serial.begin(9600); // 직렬 통신 초기화
+
+  
+  minsuCloset->setList(MinsuList);
   /*
- //led setup
+  //led setup
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT); 
   
- //lcd setup
+  //lcd setup
   lcd.init();                      // initialize the lcd 
   
   // start the SPI library:(RFID setup)
@@ -204,69 +236,68 @@ void setup(){
  
   myRFID.AddicoreRFID_Init(); 
   */
- /*data setup
-  * TODO : 
- 1.TODO : 옷장 및 코디 구성
+  /*data setup
+   * TODO : 
+   1.TODO : 옷장 및 코디 구성
   */
 
-/*
- * 1) 코디리스트 생성
- * DailyLook MinsuCloset[] = { DailyLook(String name, int* category, int temperBase, int temperLimit, int humidBase, int humidLimit),
- *                             // 민수의 코디 목록 };
- * DailyLook SuzyCloset[] = { DailyLook(String name, int* category,int*rgb, int temperBase, int temperLimit, int humidBase, int humidLimit),
- * DailyLook(String name, int* category, int temperBase, int temperLimit, int humidBase, int humidLimit),
- *                             // 수지의 코디 목록 };
- */
+  /*
+   * 1) 코디리스트 생성
+   * DailyLook MinsuCloset[] = { DailyLook(String name, int* category, int temperBase, int temperLimit, int humidBase, int humidLimit),
+   *                             // 민수의 코디 목록 };
+   * DailyLook SuzyCloset[] = { DailyLook(String name, int* category,int*rgb, int temperBase, int temperLimit, int humidBase, int humidLimit),
+   * DailyLook(String name, int* category, int temperBase, int temperLimit, int humidBase, int humidLimit),
+   *                             // 수지의 코디 목록 };
+   */
   /*
    * 2) 옷장 생성
-  Closet Minsu = new Closet("Minsu", 0x~~~, MinsuCloset); 
-  Closet Suzy = new Closet(Suzy, 0x~~~, SuzyCloset);
+   Closet Minsu = new Closet("Minsu", 0x~~~, MinsuCloset); 
+   Closet Suzy = new Closet(Suzy, 0x~~~, SuzyCloset);
   */
   /*
-  * 3) 
-  */
+   * 3) 
+   */
   
   
 }//end of setup
 
 void loop(){
-
- MinsuCloset.toString();
-
-/*
- // TODO : 날씨별 메시지 작성 
- // TODO : 추천 목록 Looks recommands[10];
- // TODO : 통신 setup
- */
- 
-/*
- * UID 입력 없을시
- */
-/*
- * 날씨를 출력한다.
- */
- 
- 
-
- /*
-  * UID입력 들어올시
+  //i2c_communication(); // 슬레이브로 데이터 요구 및 수신 데이터 처리
+  delay(1000);
+  /*
+  // TODO : 날씨별 메시지 작성 
+  // TODO : 추천 목록 Looks recommands[10];
+  // TODO : 통신 setup
   */
-/* 
- *  현재 날씨 상태 저장하고 이를 바탕으로 코디를 추천한다.
- *  UID에 따라 옷장을 구별한다.
- *  
- */
-  
-/*
- * 옷추천 +음악
- * -> 옷 추천 하는 함수 만들기
- * chooseDailyLook(Closet person, int temper, int humid)
- */ 
+ 
+  /*
+   * UID 입력 없을시
+   */
+  /*
+   * 날씨를 출력한다.
+   */
+ 
+ 
 
-/*
- * 평가
- * -> 코디를 평가하는 함수 만들기
-  scoreDailyLook(DailyLook look)
- */
+  /*
+   * UID입력 들어올시
+   */
+  /* 
+   *  현재 날씨 상태 저장하고 이를 바탕으로 코디를 추천한다.
+   *  UID에 따라 옷장을 구별한다.
+   *  
+   */
+  
+  /*
+   * 옷추천 +음악
+   * -> 옷 추천 하는 함수 만들기
+   * chooseDailyLook(Closet person, int temper, int humid)
+   */ 
+
+  /*
+   * 평가
+   * -> 코디를 평가하는 함수 만들기
+   scoreDailyLook(DailyLook look)
+  */
   
 }
